d3ff9871f01fbed639d3b4c32ad625310c201f56
Bjørn-Olav Strand <bo@startsiden.no>
2016-06-30 09:44:25 +0200

    Adding travis

---
+++ .travis/install-fuseandroid.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# https://docs.travis-ci.com/user/multi-os/
+# https://docs.travis-ci.com/user/osx-ci-environment/
+
+# http://apple.stackexchange.com/questions/72226/installing-pkg-with-terminal
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+	# In /Users/travis/build/bolav/fuse-travis
+	echo "Installing Fuse Android"
+	(echo; while true; do echo y; done) | fuse install android
+fi
